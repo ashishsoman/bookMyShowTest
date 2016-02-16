ee180ddf45d62cc5e1726016443305060f461bcc
MacScripter <Admin@macscripters-imac.local>
2016-02-16 11:28:06 +0530

    comm by mayur

--- bookMyShowTest/FirstViewController.swift
+++ bookMyShowTest/FirstViewController.swift
@@ -19,12 +19,13 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+         // mayur ddffd
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        // mayur d
+       
     }
 
 
