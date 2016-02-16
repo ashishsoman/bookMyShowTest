b1e060a95336946dd89c869d31c5232b24a4b2c1
Aashish <ashish@Kishor.local>
2016-02-16 11:31:13 +0530

    comment added by ashish

--- bookMyShowTest/FirstViewController.swift
+++ bookMyShowTest/FirstViewController.swift
@@ -23,8 +23,9 @@ class FirstViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // mayur d
         // ashish
+        
     }
 
 
